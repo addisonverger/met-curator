c701d72feefdb7ce21b3333708e7a0dfb440044a
Addison Verger <addison.j.verger@gmail.com>
2019-03-23 10:45:28 -0500

    app in progress - basic muuri functionality added to my gallery page

--- src/containers/My-Gallery.js
+++ src/containers/My-Gallery.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import MuuriGrid from 'react-muuri'
 
 import { Section } from 'react-bulma-components/full'
 
-const MyGallery = () => {
-  return (
-    <Section>
-      <div>My Gallery is rendering</div>
-    </Section>
-  )
+class MyGallery extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  componentDidMount () {
+    this.grid = new MuuriGrid({
+      node: this.gridElement,
+      defaultOptions: {
+        dragEnabled: true,
+        layout: {
+          fillGaps: true
+        }
+      },
+    });
+  }
+  componentWillUnmount () {
+    this.grid.getMethod('destroy');
+  }
+  render() {
+    return (
+      <Section>
+        <div>
+          <div className="grid" ref={gridElement => this.gridElement = gridElement}>
+            <div className="item box1">
+              <div className="item-content">
+                Box 1
+              </div>
+            </div>
+            <div className="item box2">
+              <div className="item-content">
+                Box 2
+              </div>
+            </div>
+          </div>
+        </div>
+      </Section>
+    )
+  }
 }
 
-export default MyGallery
+const mapStateToProps = (state) => ({
+
+})
+
+const mapDispatchToProps = (dispatch) => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyGallery)
