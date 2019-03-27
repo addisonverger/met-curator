88d32eb7a81e85dd630130bf7ffba86324f3cf0b
Addison Verger <addison.j.verger@gmail.com>
2019-03-26 21:25:46 -0500

    app in progress - created array of image tags

---
+++ src/Image-Tags.js
@@ -0,0 +1,210 @@
+let imageTags = [
+  {
+    name: 'Angel',
+    value: 'angel'
+  },
+  {
+    name: 'Armor',
+    value: 'armor'
+  },
+  {
+    name: 'Autumn',
+    value: 'autumn'
+  },
+  {
+    name: 'Bird',
+    value: 'bird'
+  },
+  {
+    name: 'Blue',
+    value: 'color blue'
+  },
+  {
+    name: 'Cat',
+    value: 'cat'
+  },
+  {
+    name: 'Ceramic',
+    value: 'ceramic'
+  },
+  {
+    name: 'China',
+    value: 'china'
+  },
+  {
+    name: 'Circle',
+    value: 'circle'
+  },
+  {
+    name: 'Dance',
+    value: 'dance'
+  },
+  {
+    name: 'Dark',
+    value: 'dark'
+  },
+  {
+    name: 'Dog',
+    value: 'dog'
+  },
+  {
+    name: 'Dragon',
+    value: 'dragon'
+  },
+  {
+    name: 'Dress',
+    value: 'dress'
+  },
+  {
+    name: 'Egypt',
+    value: 'egypt'
+  },
+  {
+    name: 'Enamel',
+    value: 'enamel'
+  },
+  {
+    name: 'England',
+    value: 'england'
+  },
+  {
+    name: 'Fish',
+    value: 'fish'
+  },
+  {
+    name: 'Flower',
+    value: 'flower'
+  },
+  {
+    name: 'Furniture',
+    value: 'furniture'
+  },
+  {
+    name: 'Gold',
+    value: 'gold'
+  },
+  {
+    name: 'Greece',
+    value: 'greece'
+  },
+  {
+    name: 'Green',
+    value: 'color green'
+  },
+  {
+    name: 'Horse',
+    value: 'horse'
+  },
+  {
+    name: 'India',
+    value: 'india'
+  },
+  {
+    name: 'Ink',
+    value: 'ink'
+  },
+  {
+    name: 'Italy',
+    value: 'italy'
+  },
+  {
+    name: 'Jade',
+    value: 'jade'
+  },
+  {
+    name: 'Jewelery',
+    value: 'jewelery'
+  },
+  {
+    name: 'Light',
+    value: 'light'
+  },
+  {
+    name: 'Music',
+    value: 'music'
+  },
+  {
+    name: 'Oil Painting',
+    value: 'oil paintings'
+  },
+  {
+    name: 'Paper',
+    value: 'paper material'
+  },
+  {
+    name: 'Peru',
+    value: 'peru'
+  },
+  {
+    name: 'Photograph',
+    value: 'photograph image'
+  },
+  {
+    name: 'Portrait',
+    value: 'portrait'
+  },
+  {
+    name: 'Red',
+    value: 'color red'
+  },
+  {
+    name: 'Sculpture',
+    value: 'sculpture'
+  },
+  {
+    name: 'Silk',
+    value: 'silk material'
+  },
+  {
+    name: 'Sky',
+    value: 'sky'
+  },
+  {
+    name: 'Spain',
+    value: 'spain'
+  },
+  {
+    name: 'Spring',
+    value: 'spring'
+  },
+  {
+    name: 'Square',
+    value: 'square'
+  },
+  {
+    name: 'Stained Glass',
+    value: 'stained glass'
+  },
+  {
+    name: 'Still Life',
+    value: 'still life'
+  },
+  {
+    name: 'Summer',
+    value: 'summer'
+  },
+  {
+    name: 'Textile',
+    value: 'textile'
+  },
+  {
+    name: 'Water',
+    value: 'water'
+  },
+  {
+    name: 'Watercolor',
+    value: 'watercolor'
+  },
+  {
+    name: 'Winter',
+    value: 'winter'
+  },
+  {
+    name: 'Women',
+    value: 'women'
+  },
+  {
+    name: 'Yellow',
+    value: 'color yellow'
+  }
+]
