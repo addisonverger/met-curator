55b455880690e22d98aad260b334f655dba9400e
Addison Verger <addison.j.verger@gmail.com>
2019-03-27 15:10:38 -0500

    app in progress - reformatted image modal and styling

--- src/components/Image-Modal.js
+++ src/components/Image-Modal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Icon, Modal, Card } from 'react-bulma-components/full'
+import { Icon, Modal, Image } from 'react-bulma-components/full'
 
 class ImageModal extends Component {
   constructor(props) {
@@ -22,23 +22,23 @@ class ImageModal extends Component {
           <i className="fas fa-3x fa-search-plus"></i>
         </Icon>
         <Modal show={this.state.open} onClose={() => this.toggleModal()}>
-          <Modal.Content style={{maxHeight: '100vh'}}>
-            <Card>
-              <Card.Image src={this.props.data.primaryImage}/>
-              <Card.Footer>
-                <p style={{textAlign: 'center', margin: 'auto'}}>
-                  {this.props.data.artistDisplayName !== '' ? this.props.data.artistDisplayName + '. ' : ''}
-                  {this.props.data.title !== '' ? this.props.data.title + '. ' : ''}
-                  {this.props.data.objectDate !== '' ? this.props.data.objectDate + '. ' : ''}
-                  {this.props.data.medium !== '' ? this.props.data.medium + '. ' : ''}
-                  {this.props.data.department !== '' ? this.props.data.department + '. ' : ''}
-                  <a href={this.props.data.objectURL}
-                      target='_blank'
-                      rel="noopener noreferrer">More info.</a>
-                </p>
-              </Card.Footer>
-            </Card>
-          </Modal.Content>
+          <Modal.Card>
+            <Modal.Card.Body>
+              <Image src={this.props.data.primaryImage}/>
+            </Modal.Card.Body>
+            <Modal.Card.Foot>
+              <p style={{textAlign: 'center', margin: 'auto'}}>
+                {this.props.data.artistDisplayName !== '' ? this.props.data.artistDisplayName + '. ' : ''}
+                {this.props.data.title !== '' ? this.props.data.title + '. ' : ''}
+                {this.props.data.objectDate !== '' ? this.props.data.objectDate + '. ' : ''}
+                {this.props.data.medium !== '' ? this.props.data.medium + '. ' : ''}
+                {this.props.data.department !== '' ? this.props.data.department + '. ' : ''}
+                <a href={this.props.data.objectURL}
+                    target='_blank'
+                    rel="noopener noreferrer">More info.</a>
+              </p>
+            </Modal.Card.Foot>
+          </Modal.Card>
         </Modal>
       </div>
     )
